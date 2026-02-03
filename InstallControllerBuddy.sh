422a482515f9b3d25dbf192f42206715ca162f74
Matteo Hausner <matteo.hausner@gmail.com>
2026-02-03 21:04:42 +0100

    feat: explicitely disable variable expansion for here-docs

--- InstallControllerBuddy.sh
+++ InstallControllerBuddy.sh
@@ -20,7 +20,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 set +o history
 
 banner=$(
-cat << EOF
+cat << 'EOF'
 ╔═════════════════════════════════════════════════════════════╗
 ║ █▀▀ █▀█ █▀█ ▀█▀ █▀▄ █▀█ █   █   █▀▀ █▀▄ █▀▄ █ █ █▀▄ █▀▄ █ █ ║
 ║ █   █ █ █ █  █  █▀▄ █ █ █   █   █▀▀ █▀▄ █▀▄ █ █ █ █ █ █  █  ║
@@ -230,7 +230,7 @@ function verify_signature() {
     tmp_signature_file=$(mktemp -p "$tmp_dir" -q) &&
     curl -o "$tmp_signature_file" -L "$2" &&
     local keyring_asc_content &&
-    keyring_asc_content=$(cat << EOF
+    keyring_asc_content=$(cat << 'EOF'
 -----BEGIN PGP PUBLIC KEY BLOCK-----
 
 mQINBFTLqOYBD/9fq4bD86GtCcxYZcBeSLW7ndP5siAvxNm5NGlHBdBftfdv47XD
